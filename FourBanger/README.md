8bf3a09987620d16b4c5ffdf502391b77953dfae
Nicholas Lindsay <2191893L@student.gla.ac.uk>
2016-12-06 01:00:04 +0000

    changed to predictive LL(1) parser, added complex type, started work on new rules

--- FourBanger/README.md
+++ FourBanger/README.md
@@ -1,27 +1,49 @@
 # FourBanger
 
-FourBanger is a parser for a very simple language. The aim of the project is to get familiar with formal grammers and formal languges. FourBanger simply reads a string and decides whether or not the string is a valid sentance in the language. This project is not throughly tested or rigoriusly designed -in fact the code is often inconsistent and ugly, and so there will be ways to break it.
+FourBanger is a simple calculator.
 
-## Formal Language
+# Recursive Descent Parsing
 
-S -> P -> ()
+The aim of parsing is to convert the inputted string into a meaningful series of actions and calculations. This program (and 99.9% of compilers) split's this process into two parts: lexical analysis and syntaxical analysis.
 
-## Recursive Descent Parsing
+ In the lexical analysis phase, the text input is split into more meaningful chunks called tokens. For example, the input 1234 would be converted into a NUMBER token with value 1234; "(" would be converted into an OPEN_BRACKET token and "+" would be converted into an ADD_OPERATOR token. This form simplifies the processing in the next stage.
 
-The aim is to check whether a given input string is valid in the language. This is down through parsing. The idea in a recursive descent parser is that every non-terminal symbol gets its own function. Inside each function, every production rule for the current non-terminal is checked until a valid production is found or the end of the function is reached, in which case it returns a failure code. This type of parsing can be inefficient as it may have to try several derivation trees until a valid production is encountered.
+ In the syntaxical analysis phase, the token stream is converted into a more structured representation which greater represents the meaning of the inputted text. In the case of this calculator, for instance, the syntaxical analysis examines the tokens and generates and solves the mathematical expressions that the user has inputted, taking into account the orders of precendence.
+
+ The language can be represented by a Formal Grammar. The parsers job is to try to match the inputted input string to a derivation tree (a graph which shows the structure of a particular sentance). There may be more that one grammar per formal language. The grammar below is structed in such a way that the derivation tree mimics the mathematical meaning of a sentance - operations which are performed first and lower down in the tree. As you move up the tree, every expression on each level gets computed until there is a final answer left on the root of the tree.
 
 # Formal Language
 
 The formal language for FourBanger is of the following EBNF form:
 
-start = term;
-term = factor {["+"|"-"] factor};
-factor = subfactor {["*"|"/"] subfactor};
-subfactor = "(" term ")"
-			| {"0"|"1"|"2"|"3"|"4"|"5"|"6"|"7"|"8"|"9"};
-
+<St1>:	start = term;
+<St2>:  start = ident "=" term;
+<Te1>:	term = factor {["+"|"-"] factor};
+<Fa1>:	factor = subfactor {["*"|"/"] subfactor};
+<Sf1>:	subfactor = "(" term ")"
+<Sf2>:  subfactor = number
+		number = {0-9}+
+		ident = (A-z) {(A-z)|(0-9)}+
 Here the following conventions are used:
-{...} - one or more repetitions
+{...} - zero or more repetitions
+{...}+- one or more repetiitions
 [...] - optional
 a|b	  -	choice
 "t"	  - terminal
+
+
+
+That production rules are labelled in the LHS of each line.
+
+The grammar is LL(1). This means that the next production rule can be determined by looking solely at the next input to the parser. This avoids the need for backtracking. It also means that if an invalid symbol occurs within a production it is definately an error and not an alternative production.
+
+|           | number |   "("   |   ")"  | ident |
+|:---------:|:------:|:-------:|:------:|:-----:|
+|     start |   St1  |   St1   |    -   |  St2  |
+|      term |   Te1  |   Te1   |    -   |   -   |
+|    factor |   Fa1  |   Fa1   |    -   |   -   |
+| subfactor |   Sf1  |   Sf1   |    -   |   -   |
+
+The above table shows how the transitions used by the predictive parser. Each row represents a non-terminal which is on the LHS of an expression, and each column represents the first symbol on the RHS of the expression. The content of each cell is the production rule that corresponds to each pair. A dash indicates that there are no rules with the column as the first symbol on the RHS and therefor that there is a syntax error.
+
+Note that this table is not optimal but is unlikely to be optimised until the langauge gets into any final state (which is unlikely).
--- FourBanger/main.cpp
+++ FourBanger/main.cpp
@@ -2,23 +2,132 @@
 #include <sstream>
 #include <stack>
 #include <ctype.h>
+#include <math.h>
+
+// Complex number class
+template <typename T>
+class Complex
+{
+public:
+	Complex()
+		: real(0), imag(0) {};
+	Complex(T r)
+		: real(r), imag(0) {};
+	Complex(T r, T i)
+		: real(r), imag(i) {};
+
+	// Global constants
+	static Complex<T> j()
+	{
+		return Complex<T>(0, 1);
+	}
+
+	// Overload operators
+	Complex<T> operator+(const Complex<T>& rhs) const
+	{
+		return Complex<T>(real + rhs.real, imag + rhs.imag);
+	}
+	Complex<T> operator-(const Complex<T> &rhs) const
+	{
+		return Complex<T>(real - rhs.real, imag - rhs.imag);
+	}
+	Complex<T> operator*(const Complex<T> &rhs) const
+	{
+		Complex<T> retval;
+		retval.real = real * rhs.real - imag * rhs.imag;
+		retval.imag = imag * rhs.real + real * rhs.imag;
+		return retval;
+	}
+	Complex<T> operator/(const Complex<T> &rhs) const
+	{
+		Complex<T> retval;
+		retval = (*this * rhs.congj()) / (rhs.magnitude());
+		return retval;
+	}
+	
+	// Compound assignment
+	Complex<T>& operator+=(const Complex<T> &rhs)
+	{
+		real += rhs.real;
+		imag += rhs.imag;
+		return *this;
+	}
+	Complex<T>& operator-=(const Complex<T> &rhs)
+	{
+		real -= rhs.real;
+		imag -= rhs.imag;
+		return *this;
+	}
+	Complex<T>& operator *=(const Complex<T> &rhs)
+	{
+		real = real * rhs.real - imag * rhs.imag;
+		imag = real * rhs.imag + imag * rhs.real;
+		return *this;
+	}
+	Complex<T>& operator /=(const Complex<T> &rhs)
+	{
+		*this *= rhs.congj();
+		this->real /= rhs.magnitude2();
+		this->imag /= rhs.magnitude2();
+		return *this;
+	}
+
+	// Negatation operator
+	Complex<T> operator-()
+	{
+		return Complex<T>(-real, -imag);
+	}
+
+	// Complex-specific functions
+	Complex<T> congj() const
+	{
+		return Complex<T>(real, -imag);
+	}
+
+	// Polar representation
+	T magnitude() const
+	{
+		return (sqrt(real*real + imag*imag));
+	}
+	T angle() const
+	{
+		return atan2(real, imag);
+	}
+	// Magnitude squared:
+	T magnitude2() const
+	{
+		return (*this * this->congj()).real;
+	}
+
+	// Internal representation
+	T real, imag;
+};
+
+// Typedef the complex number type
+typedef Complex<double> C_double;
+
+// Symbol types used by symbol
+enum SymbolType {
+	NUMBER,
+	ADD_OPERATOR,
+	SUB_OPERATOR,
+	MULTIPLY_OPERATOR,
+	DIVIDE_OPERATOR,
+	OPEN_BRACKET,
+	CLOSE_BRACKET,
+	EQUAL_OPERATOR,
+	END_OF_STREAM,
+	IDENTIFIER,
+	ILLEGAL
+};
 
 // Struct to hold contents of symbol
 struct Symbol
 {
-	enum SymbolType {
-		NUMBER,
-		ADD_OPERATOR,
-		SUB_OPERATOR,
-		MULTIPLY_OPERATOR,
-		DIVIDE_OPERATOR,
-		OPEN_BRACKET,
-		CLOSE_BRACKET,
-		END_OF_STREAM,
-		ILLEGAL
-	} Type;
+	enum SymbolType Type;
 
 	int NumericData;
+	std::string TextData;
 
 	Symbol(SymbolType type, int number = 0)
 		: Type(type), NumericData(number) {};
@@ -46,45 +155,63 @@ public:
 	void GetNextSym() {
 		// Check for end of file
 		if (mStream->eof()) {
-			mCurrentSym = Symbol(Symbol::END_OF_STREAM);
+			mCurrentSym = Symbol(END_OF_STREAM);
 		}
 		// Now start checking characters
 		else {
 			unsigned char cur_char;
-			*mStream >> cur_char;
+			*mStream >> std::skipws >> cur_char;
 
+			if (mStream->eof()) {
+				mCurrentSym = Symbol(END_OF_STREAM);
+			}
 			// Number:
-			if (isdigit(cur_char)) {
+			else if (isdigit(cur_char)) {
 				int numeric_value = cur_char - '0';
 				while (isdigit(mStream->peek()) && !mStream->eof()) {
 					*mStream >> cur_char;
 					numeric_value = numeric_value * 10 + (cur_char - '0');
 				}
-				mCurrentSym = Symbol(Symbol::NUMBER, numeric_value);
+				mCurrentSym = Symbol(NUMBER, numeric_value);
 			}
 			// Operators
 			else if (cur_char == '+') {
-				mCurrentSym = Symbol(Symbol::ADD_OPERATOR);
+				mCurrentSym = Symbol(ADD_OPERATOR);
 			}
 			else if (cur_char == '-'){
-				mCurrentSym = Symbol(Symbol::SUB_OPERATOR);
+				mCurrentSym = Symbol(SUB_OPERATOR);
 			}
 			else if (cur_char == '*'){
-				mCurrentSym = Symbol(Symbol::MULTIPLY_OPERATOR);
+				mCurrentSym = Symbol(MULTIPLY_OPERATOR);
 			}
 			else if (cur_char == '/'){
-				mCurrentSym = Symbol(Symbol::DIVIDE_OPERATOR);
+				mCurrentSym = Symbol(DIVIDE_OPERATOR);
 			}
 			// Brackets
 			else if (cur_char == '('){
-				mCurrentSym = Symbol(Symbol::OPEN_BRACKET);
+				mCurrentSym = Symbol(OPEN_BRACKET);
 			}
 			else if (cur_char == ')'){
-				mCurrentSym = Symbol(Symbol::CLOSE_BRACKET);
+				mCurrentSym = Symbol(CLOSE_BRACKET);
+			}
+			// Letter (identifier):
+			else if (isalpha(cur_char)) {
+				// Disable ws until we get full identifer
+				*mStream >> std::noskipws;
+
+				mCurrentSym = Symbol(IDENTIFIER);
+
+				while (cur_char != ' ' && !mStream->eof()){
+					mCurrentSym.TextData += cur_char;
+					*mStream >> cur_char;
+				}
+
+				// Re-enable ws
+				*mStream >> std::skipws;
 			}
 			// Unrecognised
 			else {
-				mCurrentSym = Symbol(Symbol::ILLEGAL);
+				mCurrentSym = Symbol(ILLEGAL);
 			}
 		}
 		
@@ -95,10 +222,10 @@ private:
 	Symbol mCurrentSym;
 };
 
-// Returns 1 if symbol matches, zero otherwise
-int accept(TokenStream* str, Symbol s)
+// Returns 1 if symbol matches and consumes symbol, zero otherwise
+int accept(TokenStream* str, SymbolType s)
 {
-	if (str->CurrentSym().Type == s.Type) {
+	if (str->CurrentSym().Type == s) {
 		str->GetNextSym();
 		return 1;
 	}
@@ -106,7 +233,7 @@ int accept(TokenStream* str, Symbol s)
 }
 
 // Similar to above but gives error message
-int expect(TokenStream* str, Symbol s)
+int expect(TokenStream* str, SymbolType s)
 {
 	if (accept(str, s))
 		return 1;
@@ -117,110 +244,186 @@ int expect(TokenStream* str, Symbol s)
 }
 
 // Function prototypes
-int term(TokenStream* str, int* number);
+int term(TokenStream* str, C_double* number);
+
+// Parse the start symbol and evaluates expression. 1 if succesful, 0 otherwise
+int start(TokenStream* str, C_double* number)
+{
+	switch (str->CurrentSym().Type)
+	{
+	case NUMBER:
+	case OPEN_BRACKET:
+	{
+		if (term(str, number)) {
+			if (str->CurrentSym().Type == END_OF_STREAM) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}
+		break;
+	}
+	case IDENTIFIER:
+	{
+		// Insert code here to remember identifier name
+		if (expect(str, EQUAL_OPERATOR)) {
+			C_double temp_no;
+			if (term(str, &temp_no)) {
+				// Insert code here to update symbol table
+			}
+			else {
+				return 0;
+			}
+		}
+		else
+		{
+			return 0;
+		}
+		break;
+	}
+	default:
+	{
+		std::cout << "Syntax error. Expected a term.\n";
+		return 0;
+	}
+	}
+}
 
 // Parse a subfactor and evaluate expression. Returns 1 if successful or 0 if
 // input not accepted
-int subfactor(TokenStream* str, int* number)
+int subfactor(TokenStream* str, C_double* number)
 {
-	Symbol currentSym = str->CurrentSym();
+	C_double local_no(0, 0);
 
-	// subfactor -> number
-	if (accept(str, Symbol(Symbol::NUMBER))) {
-		*number = currentSym.NumericData;
+	switch (str->CurrentSym().Type)
+	{
+	case NUMBER: {
+		*number = str->CurrentSym().NumericData;
+		str->GetNextSym();
 		return 1;
+		break;
 	}
-	// subfactor -> "(" term ")"
-	if (accept(str, Symbol(Symbol::OPEN_BRACKET))) {
-		if (term(str, number)) {
-			return (expect(str, Symbol(Symbol::CLOSE_BRACKET)));
+	case OPEN_BRACKET: {
+		str->GetNextSym();
+		if (term(str, &local_no)) {
+			if (expect(str, CLOSE_BRACKET)) {
+				*number = local_no;
+				return 1;
+			}
+			else {
+				return 0;
+			}
 		}
 		else {
-			std::cout << "Syntax error. Expected term.\n";
+			std::cout << "Error. Expected a term.\n";
 			return 0;
 		}
+		break;
 	}
-
-	// no match
-	else return 0;
+	default: {
+		std::cout << "Unexpected symbol.\n";
+		return 0;
+	}
+	}
 }
 
 // Parse a factor and evaluate result. Returns 1 if successful or 0 if error
-int factor(TokenStream* str, int* number)
+int factor(TokenStream* str, C_double* number)
 {
-	Symbol currentSym = str->CurrentSym();
-	int eval_val = 0;
+	C_double local_no(0, 0);
 
-	// factor -> subfactor {[*]|[/] subfactor}
-	if (subfactor(str, &eval_val)) {
-		while (1) {
-			if (accept(str, Symbol(Symbol::MULTIPLY_OPERATOR))) {
-				int sub_val;
-				if (subfactor(str, &sub_val)) {
-					eval_val *= sub_val;
+	switch (str->CurrentSym().Type)
+	{
+	case NUMBER:
+	case OPEN_BRACKET:
+	{
+		if (subfactor(str, &local_no))
+		{
+			C_double temp_no;
+
+			while (1) {
+				if (accept(str, MULTIPLY_OPERATOR)) {
+					if (subfactor(str, &temp_no)) {
+						local_no *= temp_no;
+					}
+					else {
+						return 0;
+					}
+				}
+				else if (accept(str, DIVIDE_OPERATOR)) {
+					if (subfactor(str, &temp_no)) {
+						local_no /= temp_no;
+					}
+					else {
+						return 0;
+					}
 				}
 				else {
-					std::cout << "Unexpected symbol!\n";
-					return 0;
+					*number = local_no;
+					return 1;
 				}
 			}
-			else if (accept(str, Symbol(Symbol::DIVIDE_OPERATOR))) {
-				int sub_val;
-				if (subfactor(str, &sub_val)) {
-					eval_val /= sub_val;
-				}
-				else {
-					std::cout << "Unexpected symbol!\n";
-					return 0;
-				}
-			}
-			else {
-				*number = eval_val;
-				return 1;
-			}
+			
 		}
-		
+		else {
+			return 0;
+		}
+		break;
 	}
-	// not a valid production:
-	else return 0;
+	default:
+	{
+		std::cout << "Unexpected symbol.\n";
+		return 0;
+	}
+	}
 }
 
-// Parse a term and evaluate return. Returns 1 if successful or 0 if error
-int term(TokenStream* str, int* number)
+// Parse a term and evaluate result. Returns 1 if successful or 0 if error
+int term(TokenStream* str, C_double* number)
 {
-	int term_value = 0;
+	C_double local_no(0, 0);
 
-	if (factor(str, &term_value)){
-		while (1){
-			if (accept(str, Symbol::ADD_OPERATOR)){
-				int factor_val = 0;
-				if (factor(str, &factor_val)) {
-					term_value += factor_val;
+	switch (str->CurrentSym().Type)
+	{
+	case NUMBER:
+	case OPEN_BRACKET:
+	{
+		if (factor(str, &local_no)) {
+			while (1) {
+				C_double temp_no;
+				if (accept(str, ADD_OPERATOR)) {
+					if (factor(str, &temp_no)) {
+						local_no += temp_no;
+					}
+					else {
+						return 0;
+					}
+				}
+				else if (accept(str, SUB_OPERATOR)) {
+					if (factor(str, &temp_no)) {
+						local_no -= temp_no;
+					}
+					else {
+						return 0;
+					}
 				}
 				else {
-					std::cout << "Syntax error. Expected a factor!\n";
-					return 0;
+					*number = local_no;
+					return 1;
 				}
-			}
-			else if (accept(str, Symbol::SUB_OPERATOR)){
-				int factor_val = 0;
-				if (factor(str, &factor_val)) {
-					term_value -= factor_val;
-				}
-				else {
-					std::cout << "Syntax error. Expected a factor!\n";
-					return 0;
-				}
-			}
-			else {
-				*number = term_value;
-				return 1;
 			}
 		}
+		else {
+			return 0;
+		}
+		break;
 	}
-	else {
-		std::cout << "Syntax error. Expected a factor!\n";
+	default:
+	{
+		std::cout << "Unexpected symbol.\n";
 		return 0;
+	}
 	}
 }
 
@@ -228,18 +431,22 @@ int main()
 {
 	std::cout << "Please enter an expression: ";
 	std::string expr;
-	std::cin >> expr;
+	std::getline(std::cin, expr);
 
 	std::stringstream ss(expr);
 	TokenStream token_str(&ss);
-	int value = 0;
+	C_double value = 0;
 	
-	if (term(&token_str, &value)) {
-		std::cout << "Accepted! Value: " << value << ".\n";
+	if (start(&token_str, &value)) {
+		std::cout << "Accepted! Value: " << value.real << " + " << value.imag << "j.\n";
 	} else {
 		std::cout << "Not accepted.\n";
 	}
 	
+	Complex<double> c1(1, 3);
+	Complex<double> c2(2, -1);
+	auto c3 = c1 * c2 *  0;
+
 	std::getchar();
 	std::getchar();
 }
